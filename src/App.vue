b5851d83dd2bc925dd1a8cc872a8be1bbe080078
Yan Fernandes <yanfernandes404@gmail.com>
2023-07-17 23:17:59 -0300

    Feat: Carrinho responsivo
    Header responde quanto itens existem dentro do carrinho

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Início</router-link> -
-    <router-link to="/basket">Carrinho (0)</router-link> -
+    <router-link to="/basket">Carrinho {{ this.productsInBag.length }}</router-link> -
   </div>
   <router-view/>
 </template>
@@ -10,7 +10,12 @@
   export default {
     created(){
       this.$store.dispatch('loadProducts');
-    
+    },
+
+    computed:{
+      productsInBag(){
+        return this.$store.state.productsInBag;
+      }
     }
   }
   
